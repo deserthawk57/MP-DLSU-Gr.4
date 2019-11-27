14064970936a634baa83a52190cf1f956000e152
deserthawk57 <58214726+deserthawk57@users.noreply.github.com>
2019-11-27 13:07:31 +0800

    Asset source change
    change thse asset source

--- Rocket.gd
+++ Rocket.gd
@@ -6,6 +6,5 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
+	#$Rocket_Sprite.rotation += 1
