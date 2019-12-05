6e365c9ce7499e95bee9cf340e70b18f61dc8128
AlvaNoel <58319597+AlvaNoel@users.noreply.github.com>
2019-12-06 03:52:08 +0800

    Add files via upload
    Objects spawn on arc now; the if statements on pos.x tries to make it so no object spawns on top of each other.

--- Spawner_obstacle.gd
+++ Spawner_obstacle.gd
@@ -3,20 +3,33 @@ extends Node
 onready var obstacle_asteroid = preload("res://asteroid.tscn")
 var spawn = 10
 var rand
-
+var y = 0
 func _ready():
 	spawn()
 	pass 
 
+
 func spawn():
+	# ((x-h)*(x-h)) + k, LLx = 0, HLx= 1010
 	randomize()
-	rand = rand_range(0,100)
+	
 	while spawn > 0:
 		var obstacle1 = obstacle_asteroid.instance()
 		var pos = Vector2()
-		pos.y = rand_range(50, 400)
-		pos.x = rand_range(100, 900)
+		var x = rand_range(10,1010)
+		if x <= 10 and x <= 200:
+			pos.x = x + y
+		elif x <= 201 and x <= 402:
+			pos.x = x + 2 * y
+		elif x <= 403 and x <= 604:
+			pos.x = x
+		elif x <= 605 and x <= 806:
+			pos.x = x - 2 * y
+		else:
+			pos.x = x - y
+		pos.y = ((x-525)*(x-525))/750 + 75
 		obstacle1.set_position(pos)
 		get_node("container").add_child(obstacle1)
 		spawn -= 1
+		var y = x
 	
