c07d09774e10283cf1c147cc40443b347ea3dac4
AlvaNoel <58319597+AlvaNoel@users.noreply.github.com>
2019-12-13 04:50:23 +0800

    Add files via upload

--- Spawner_obstacle.gd
+++ Spawner_obstacle.gd
@@ -4,6 +4,8 @@ const RngGd = "res://NoRepeatRNG.gd"
 onready var obstacle_asteroid = preload("res://asteroid.tscn")
 onready var obstacle_star = preload("res://star.tscn")
 onready var obstacle_blackhole = preload("res://BlackHole.tscn")
+var difficulty = 1
+
 var spawn = 10
 var rand
 var y = 0
@@ -17,6 +19,7 @@ func spawn():
 	randomize()
 	
 	while spawn > 0:
+		
 		var obstacle1 = obstacle_asteroid.instance()
 		var obstacle2 = obstacle_star.instance()
 		var obstacle3 = obstacle_blackhole.instance()
@@ -25,15 +28,27 @@ func spawn():
 		preload("NoRepeatRNG.gd").new(x)
 		pos.x = x
 		pos.y = ((x-525)*(x-525))/750 + 75
-		if spawn == 5:
-			obstacle2.set_position(pos)
-			get_node("container").add_child(obstacle2)
-		elif spawn == 6:
-			obstacle3.set_position(pos)
-			get_node("container").add_child(obstacle3)
-		elif spawn != 5:
+		if difficulty == 1:
 			obstacle1.set_position(pos)
 			get_node("container").add_child(obstacle1)
+		elif difficulty < 5:
+			var z = int(rand_range(1,11))
+			if z == 1:
+				obstacle2.set_position(pos)
+				get_node("container").add_child(obstacle2)
+			else:
+				obstacle1.set_position(pos)
+				get_node("container").add_child(obstacle1)
+		elif difficulty > 6:
+			var z = int(rand_range(1,11))
+			if z == 1:
+				obstacle2.set_position(pos)
+				get_node("container").add_child(obstacle2)
+			elif z == 2:
+				obstacle3.set_position(pos)
+				get_node("container").add_child(obstacle3)
+			else:
+				obstacle1.set_position(pos)
+				get_node("container").add_child(obstacle1)
 		spawn -= 1
 		var y = x
-	
