acffc0c3ee9be33a9f64a140e8ffb370fc2c4369
AlvaNoel <58319597+AlvaNoel@users.noreply.github.com>
2019-12-13 21:28:21 +0800

    Add files via upload

--- Main.gd
+++ Main.gd
@@ -3,6 +3,7 @@ extends Node2D
 var lives = global.lives
 var start_pos = Vector2(0,-15)
 var rocket
+
 func _ready():
 	rocket = get_node('/root/Main/Rocket/KinematicBody2D')
 	rocket.connect('rocket_die',self,"restart")
@@ -15,5 +16,6 @@ func restart():
 	
 	#$Rocket.set_position(start_pos)
 	rocket.set_position(start_pos)
-	
+	global.difficulty +=1
+	get_tree().reload_current_scene()
 	
