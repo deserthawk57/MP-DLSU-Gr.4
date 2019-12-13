b490c175d010047c3b72f932ed9f91de43c613f3
jlurrutia2001 <35764481+jlurrutia2001@users.noreply.github.com>
2019-12-13 22:26:00 +0800

    Add files via upload
    Updated with new animation and timer feature for animation effect, also added stop rocket when collide with boom

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -3,49 +3,41 @@ extends KinematicBody2D
 var User_interface = preload ("res://User_Interface.tscn")
 var velocity = Vector2()
 var speed = 200
-signal rocket_die
 
+signal rocket_die
+ 
+onready var effects_animation = $EffectsAnimation
+onready var damage_timer = $DamageTimer
+
+#health
+var health = 4
+var damaged
 
 func _ready():
 	get_node('/root/Main/UI/Panel').connect('rocket_go',self,'rotation_dir')
+	get_node('HUDCanvasLayer').update(4)
 
+func hurt(amount = 1):
+	if damage_timer.is_stopped():
+		damage_timer.start()
+		health -= amount
+		get_node("HUDCanvasLayer").update(health)
+		effects_animation.play("Damage")
+		effects_animation.queue("Flash")
+	 
+func boom(amount = 4):
+	health -= amount
+	get_node("HUDCanvasLayer").update(health)
+	set_physics_process(false)
+	
 func rotation_dir(dir):
 	
 	rotation = deg2rad(int(dir)-90)
 	velocity = Vector2(0,-speed).rotated(rotation)
 	set_physics_process(true)
-	
+
 func _physics_process(delta):
 	var collision_info = move_and_collide(velocity * delta)
 
-	if collision_info:
-		var collision_point = collision_info.position
-		emit_signal('rocket_die')
-		print(collision_info.collider)
-		velocity = Vector2()
-		set_physics_process(false)
-		
-		
-
-#var speed = 200
-
-#var velocity = Vector2()
-#var rotation_speed = 1.5
-#var rotation_dir = 0
-#
-#func get_input():
-#    rotation_dir = 0
-#    velocity = Vector2()
-#    if Input.is_action_pressed('ui_right'):
-#        rotation_dir += 1
-#    if Input.is_action_pressed('ui_left'):
-#        rotation_dir -= 1
-#    if Input.is_action_pressed('ui_down'):
-#        velocity = Vector2(0, speed).rotated(rotation)
-#    if Input.is_action_pressed('ui_up'):
-#        velocity = Vector2(0, -speed).rotated(rotation)
-#
-#func _physics_process(delta):
-#    get_input()
-#    rotation += rotation_dir * rotation_speed * delta
-#    velocity = move_and_slide(velocity)
+func _on_DamageTimer_timeout():
+	effects_animation.play("Rest")
