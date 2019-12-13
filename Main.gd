bd731f325ef2d65515b5a580bb40ae0db38ea6a0
AlvaNoel <58319597+AlvaNoel@users.noreply.github.com>
2019-12-13 22:16:28 +0800

    Add files via upload

--- Main.gd
+++ Main.gd
@@ -3,6 +3,7 @@ extends Node2D
 var lives = global.lives
 var start_pos = Vector2(0,-15)
 var rocket
+var reload_obstacles = load('res://Spawner_obstacle.gd').new()
 
 func _ready():
 	rocket = get_node('/root/Main/Rocket/KinematicBody2D')
@@ -17,5 +18,8 @@ func restart():
 	#$Rocket.set_position(start_pos)
 	rocket.set_position(start_pos)
 	global.difficulty +=1
-	get_tree().reload_current_scene()
-	
+	var last_round = get_node("Spawner_obstacle")
+	remove_child(last_round)
+	last_round.call_deferred("free")
+	var next_round = preload("Spawner_obstacle.tscn").instance()
+	add_child(next_round)
